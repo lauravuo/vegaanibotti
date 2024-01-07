bcc9e69df39b60c82721b89d01b717fb794a16ed
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2024-01-07 19:45:45 +0200

    Add more logs.

--- auth/auth.go
+++ auth/auth.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"log/slog"
 	"net/url"
 	"os"
 
@@ -17,6 +18,12 @@ type Response struct {
 	AccessToken string `json:"access_token"`
 	//nolint:tagliatelle
 	RefreshToken string `json:"refresh_token"`
+	//nolint:tagliatelle
+	Type string `json:"token_type"`
+	//nolint:tagliatelle
+	Expires int `json:"expires_in"`
+	//nolint:tagliatelle
+	Scope string `json:"scope"`
 }
 
 func FetchAccessToken(clientID, clientSecret, refreshToken, endpoint string) string {
@@ -35,10 +42,11 @@ func FetchAccessToken(clientID, clientSecret, refreshToken, endpoint string) str
 	if err == nil {
 		response := Response{}
 		if err = json.Unmarshal(data, &response); err == nil {
+			slog.Info("Acquired new X tokens", "type", response.Type, "scope", response.Scope, "expires", response.Expires)
 			// happy end: token parsed successfully
 			try.To(os.WriteFile(
 				"./data/.envrc",
-				[]byte("export NEW_X_REFRESH_TOKEN="+response.RefreshToken),
+				[]byte("export NEW_X_REFRESH_TOKEN=\""+response.RefreshToken+"\""),
 				base.WritePerm))
 
 			return response.AccessToken
