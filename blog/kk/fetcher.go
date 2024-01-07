398c21bedae4b0a0ff7c19c0acd06f60a0cfcced
Laura Vuorenoja <laura.vuorenoja@gmail.com>
2024-01-07 21:09:35 +0200

    Add kasviskapina blog as source.

---
+++ blog/kk/fetcher.go
@@ -0,0 +1,122 @@
+package kk
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/lainio/err2/try"
+	"github.com/lauravuo/vegaanibotti/blog/base"
+	"github.com/lauravuo/vegaanibotti/myhttp"
+	"golang.org/x/text/cases"
+	"golang.org/x/text/language"
+)
+
+const RecipesPath = base.DataPath + "/kk/recipes.json"
+
+const UsedIDsPath = base.DataPath + "/kk/used.json"
+
+type Recipe struct {
+	ID            string `json:"id"`
+	Title         string `json:"title"`
+	Excerpt       string `json:"excerpt"`
+	Slug          string `json:"slug"`
+	FeaturedImage struct {
+		Nodes struct {
+			Src string `json:"sourceUrl"`
+		} `json:"node"`
+	} `json:"featuredImage"`
+	Categories struct {
+		Nodes []struct {
+			Name string `json:"name"`
+			Slug string `json:"slug"`
+		} `json:"nodes"`
+	} `json:"categories"`
+}
+
+type Response struct {
+	PageProps struct {
+		Posts []Recipe `json:"posts"`
+	} `json:"pageProps"`
+}
+
+func (r *Recipe) ToPost() base.Post {
+	// ID
+	strID := try.To1(base64.StdEncoding.DecodeString(r.ID))
+	postID := try.To1(strconv.ParseInt(strings.Split(string(strID), ":")[1], 10, 64))
+
+	// Title
+	var titleBytes []byte
+
+	caser := cases.Title(language.Finnish)
+	_, _ = try.To2(caser.Transform([]byte(r.Title), titleBytes, true))
+
+	// Description
+	startIndex := strings.Index(r.Excerpt, ">")
+	endIndex := strings.LastIndex(r.Excerpt, "<")
+
+	const baseImageURL = "https://www.kasviskapina.fi/_next/image?url=https://kasviskapinastor.blob.core.windows.net/images"
+
+	return base.Post{
+		ID:           postID,
+		Title:        string(titleBytes),
+		Description:  r.Excerpt[startIndex:endIndex],
+		ThumbnailURL: fmt.Sprintf("%s%s&w=384&q=75", baseImageURL, r.FeaturedImage.Nodes.Src),
+		ImageURL:     baseImageURL + r.FeaturedImage.Nodes.Src + "&w=1920&q=75",
+		URL:          "https://kasviskapina.fi/reseptit/" + r.Slug,
+		Added:        true,
+		Hashtags:     []string{"kasviskapina", "vegaani", "vegaaniresepti"},
+	}
+}
+
+func FetchNewPosts(
+	recipesFilePath string,
+	_ func(string, string) ([]byte, error),
+	previewOnly bool,
+) (base.RecipeBank, error) {
+	// No pagination currently?, fetch all at once
+	res := try.To1(
+		myhttp.DoGetRequest("https://www.kasviskapina.fi/_next/data/9LbFsI_aPTZdpR7caehIE/fi/kategoriat/reseptit.json?slug=paaruoka", ""),
+	)
+
+	var apiResponse Response
+
+	try.To(json.Unmarshal(res, &apiResponse))
+
+	posts := make([]base.Post, 0)
+
+	for _, receipt := range apiResponse.PageProps.Posts {
+		for _, category := range receipt.Categories.Nodes {
+			if category.Slug == "paaruoka" {
+				posts = append(posts, receipt.ToPost())
+
+				break
+			}
+		}
+	}
+
+	sort.Slice(posts, func(i, j int) bool {
+		return posts[i].ID > posts[j].ID
+	})
+
+	slog.Info("Found posts for kk", "count", len(posts))
+
+	if !previewOnly {
+		buffer := &bytes.Buffer{}
+		encoder := json.NewEncoder(buffer)
+		encoder.SetEscapeHTML(false)
+		try.To(encoder.Encode(posts))
+		try.To(os.WriteFile(recipesFilePath, buffer.Bytes(), base.WritePerm))
+	}
+
+	return base.RecipeBank{
+		Posts:       posts,
+		UsedIDsPath: UsedIDsPath,
+	}, nil
+}
